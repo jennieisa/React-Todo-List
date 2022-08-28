9b8e0ee23c874233b59e482f23932b44a9237034
jennieisa <jennieisakssoon97@gmail.com>
2022-08-28 17:04:27 +0200

    creating the function that is marking a todo done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState } from 'react';
 import TodoList from './components/TodoList.jsx';
-import NewTodo from './components/NewTodo.jsx';
 
 function App() {
 
@@ -60,13 +59,52 @@ function App() {
     ]
   );
 
+  const findElements = () => {
+    let inputTitle = document.querySelector('#titleTodo'); 
+    let inputDesc = document.querySelector('#descTodo');
+
+    const inputs =[inputTitle.value, inputDesc.value];
+
+    return inputs;
+  }
+
+  const addNewTodo = () => {
+
+    const inputs = findElements();
+
+    console.log(inputs)
+
+    let newTodo = {
+      title: inputs[0],
+      desc: inputs[1],
+      done: false
+    }
+
+    console.log(newTodo);
+
+    setMyTodos([...myTodos, newTodo]);
+  }
+
+  const handleEvent = (e) => {
+    e.preventDefault();
+}
+
+
   console.log(myTodos);
 
   return (
     <div className="App">
       <header className="App-header">
         <h3>Todos:</h3>
-        <NewTodo addNewTodo={setMyTodos}/>
+        <form>
+            <label htmlFor="titleTodo">Titel:</label>
+            <input type="text" id="titleTodo"/>
+            <label htmlFor="descTodo">Beskrivning:</label>
+            <input type="text" id="descTodo" />
+            <button onClick={(e) => {handleEvent(e); addNewTodo();}} type="submit">
+                Lägg till
+            </button>
+        </form>
         <TodoList todos={myTodos}/>
       </header>
     </div>
--- my-app/src/components/Todo.jsx
+++ my-app/src/components/Todo.jsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
+
 const Todo = ({title, description, done, index}) => {
+    const [checked, setChecked] = useState(true);
     
     return (
         <li key={index} className={done? 'todoDone' : 'todoUndone'}>
             <p>{title}</p>
             {done && <p>{description}</p>}
+            <input type='checkbox' defaultChecked={done && checked} onChange={() => setChecked(!checked)}/>
         </li>
     )
 }
